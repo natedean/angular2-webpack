3c8a40fe2db5059f94d020ba386b27cb0eaffa8b
Nate Dean <nate.dean@pathar.net>
2016-05-04 15:41:01 -0500

    Killed a lot of stuff that isn't essential to an MVP build. This is an ideal starting place for tutorials and other bare-bones situations

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,32 +1,8 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {FORM_PROVIDERS} from 'angular2/common';
 
-import '../style/app.scss';
-
-import {Api} from './services/api/api';
-import {Home} from './components/home/home';
-import {About} from "./components/about/about";
-
-/*
- * App Component
- * Top Level Component
- */
 @Component({
-  selector: 'app', // <app></app>
-  providers: [...FORM_PROVIDERS, Api],
-  directives: [...ROUTER_DIRECTIVES],
+  selector: 'app',
   pipes: [],
-  styles: [require('./app.scss')],
-  template: require('./app.html')
+  template: `<h1>DUDEMAN!</h1>`
 })
-@RouteConfig([
-  {path: '/', component: Home, name: 'Home'},
-  {path: '/About', component: About, name: 'About'}
-])
-export class App {
-  url: string = 'https://github.com/preboot/angular2-webpack';
-
-  constructor(public api: Api) {
-  }
-}
+export class App {}
