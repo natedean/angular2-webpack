0a7ac0853a47e48c2f4e17b9724bb6457da8ff73
Nate Dean <nate.dean@pathar.net>
2016-05-14 14:47:43 -0500

    More observable tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,20 @@
 import {Component} from 'angular2/core';
 import {Observable} from "rxjs/Observable";
 
+let stream$ = new Observable((observer) => {
+    let count = 0;
+
+    let interval = setInterval(() => {
+        count = count + 1;
+        observer.next(count);
+    }, 1000);
+
+    return () => {
+        clearInterval(interval);
+    }
+});
+
+
 let stream1$ = new Observable(observer => {
     setTimeout(() => {
         observer.next('observable timeout');
@@ -13,32 +27,42 @@ let getPromiseMessage = new Promise(resolve => {
     }, 3000);
 });
 
+let mapNumberToString = (x: number) : string => x % 2 === 0 ? x + ' is even' : x + ' is odd!';
+
 @Component({
   selector: 'app',
   template: ` <h1 *ngIf="!observableMessage">LOADING THE OBSERVABLE</h1>
               <h1 *ngIf="observableMessage">{{ observableMessage }}</h1>
               <br>
               <h1 *ngIf="!promiseMessage">LOADING THE PROMISE</h1>
-              <h1 *ngIf="promiseMessage">{{ promiseMessage }}</h1>`
+              <h1 *ngIf="promiseMessage">{{ promiseMessage }}</h1>
+              <br>
+              <h3>This is an observable counter with an async pipe. From scratch. Counter: {{ stream | async }}</h3>
+              `
 })
 export class App {
     observableMessage;
     promiseMessage;
     stream1Subscription;
+    stream;
+    streamSubscription;
 
     constructor() {
         this.stream1Subscription = stream1$.subscribe((data) => {
             this.observableMessage = data;
         });
 
-        setTimeout(() => {
-            this.stream1Subscription.unsubscribe();
-        }, 1000);
-
         getPromiseMessage.then((data) => {
             this.promiseMessage = data;
         });
 
+        this.stream =  stream$
+                .startWith(0)
+                .map(mapNumberToString);
+
+        setTimeout(() => {
+            this.streamSubscription.unsubscribe();
+        }, 5000);
     }
 
 
